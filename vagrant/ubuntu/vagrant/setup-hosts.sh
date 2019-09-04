152f58a018a7f1662699b4a09450d65e86f3b420
Salvatore Castagna <salvatore.castagna@alliance-healthcare.net>
2019-09-04 18:05:14 +0200

    Update rync configuration

--- vagrant/ubuntu/vagrant/setup-hosts.sh
+++ vagrant/ubuntu/vagrant/setup-hosts.sh
@@ -6,18 +6,3 @@ HOSTNAME=$(hostname)
 cat >> /etc/hosts <<EOF
 $HOSTNAME $ADDRESS
 EOF
-#sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
-
-# remove ubuntu-bionic entry
-#sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
-
-# Update /etc/hosts about other hosts
-#cat >> /etc/hosts <<EOF
-#172.18.96.11  master-1
-#172.18.96.12  master-2
-#172.18.96.21  worker-1
-#172.18.96.22  worker-2
-#172.18.96.30  lb
-#EOF
-
-
